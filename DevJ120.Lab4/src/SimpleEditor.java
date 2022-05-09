7c2f78ef8f18bb33e8429af8588895d3a5b6125b
Bezdetko <bezdetk0@mail.ru>
2022-05-09 16:48:04 +0300

    v.01

--- DevJ120.Lab4/src/SimpleEditor.java
+++ DevJ120.Lab4/src/SimpleEditor.java
@@ -8,13 +8,184 @@
  *
  * @author bezdetk0@mail.ru
  */
-public class SimpleEditor {
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+/**
+* Класс представляет главное окно приложения, реализующего простой текстовый
+* редактор.Класс обеспечивает возможность открытия или создания файла,
+* содержащего простой текст, например, в формате .txt, .html или xml, с
+* возможностью редактирования его содержания.
+*
+* @author (C)Y.D.Zakovryashin, 11.11.2020
+*/
+public class SimpleEditor extends JFrame {
+private Container cp;
+/**
+* Надпись, отображающая имя текущего/редактируемого файла, а также метку,
+* отражающую его текущее состояние. Например, если текст изменён, но не
+* сохранён, то в данном поле ставиться специальный маркер (по выбору
+* программиста) или меняется шрифт отображения имени файла.
+*/
+private JLabel fileName;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+/**
+* Окно редактирования, в котором отображается и редактируется текст файла.
+*/
+private JTextArea text;
+
+/**
+* Главное меню приложения.
+*/
+private JMenuBar bar;
+
+/**
+* Массив пунктов главного меню приложения, который должен включать "File" и
+* "Edit".
+*/
+private JMenu[] menu;
+
+/**
+* Массив команд главного меню приложения, который должен включать команды
+* "Open", "Save", "Cancel" и "Exit".
+*/
+private JMenuItem[] commandMenu;
+
+/**
+* Массив кнопок интерфейса "Open", "Save", "Cancel" и "Exit", которые
+* дублируют команды главного меню приложения .
+*/
+private JButton[] commandButton;
+
+/**
+* Ссылка на обработчик команд пользовательского интерфейса.
+*/
+private SimpleEditorListener listener;
+
+/**
+* Конструктор приложения.
+*/
+protected SimpleEditor() {
+    setTitle("Simple text editor");
+    setSize(800,600);
+    setLocation(50, 50);
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    init();
+    createMenu();
+    setVisible(true);
+}
+
+/**
+* Стартовый метод приложения.
+*
+* @param args параметры командной строки.
+*/
+public static void main(String[] args) {
+SimpleEditor simpleEditor = new SimpleEditor();
+}
+
+/**
+* Метод инициализирует обработчик событий listener, создаёт и настраивает
+* все элементы пользовательского интерфейса.
+*/
+private void init() {
+// Реализация метода
+    cp=getContentPane();
+    cp.setLayout(new BorderLayout());
+    JPanel buttonsPanel= new JPanel(new GridLayout(1, 4, 5, 0));
+//    JPanel buttonsPanel= new JPanel(new FlowLayout(FlowLayout.CENTER));
+    cp.add(buttonsPanel, BorderLayout.NORTH);
     
+    listener = new SimpleEditorListener(this);    
+    
+    
+    commandButton = new JButton[]{
+        new JButton("Open"),
+        new JButton("Save"),
+        new JButton("Cancel"),
+        new JButton("Exit")
+    };
+    for (JButton c : commandButton){
+            buttonsPanel.add(c);
+            }
+    
+    fileName = new JLabel();
+    fileName.setText("INFO: creation new file. New File Name:");
+    cp.add(fileName, BorderLayout.SOUTH);
+
+
+    text = new JTextArea();
+    cp.add(new JScrollPane(text), BorderLayout.CENTER);
+    
+    createMenu();
+}
+
+/**
+* Метод полностью создаёт главное меню приложения и добавляет его в главное
+* окно приложения.
+*/
+private void createMenu() {
+// Реализация метода
+    bar = new JMenuBar();
+    menu = new JMenu[] {
+        new JMenu("File"),
+        new JMenu("Edit")};
+    for (JMenu m : menu){
+                bar.add(m);
+                }
+
+    commandMenu = new JMenuItem[]{
+        new JMenuItem("Open"),
+        new JMenuItem("Save"),
+        new JMenuItem("Cancel"),
+        new JMenuItem("Exit")
+    };
+
+    commandMenu[0].addActionListener(listener);
+    commandMenu[0].setActionCommand("open");
+    
+    
+    menu[0].add(commandMenu[0]);
+    menu[0].add(commandMenu[3]);
+    menu[1].add(commandMenu[1]);
+    menu[1].add(commandMenu[2]);
+
+    setJMenuBar(bar);
+}
+
+/**
+* Метод обеспечивает добавление или замену текста в окне редактирования.
+*
+* @param str добавляемый текст.
+* @param append значение true означает, что текст добавляется к тексту,
+* который содержится в поле. Значение false означает, что текст в поле
+* полностью заменяется на значение str.
+*/
+void appendText(String str, boolean append) {
+        if (append) text.setText("");
+        text.append(str);
+// Реализация метода
+}
+
+/**
+* Метод возвращает весь текст, содержащийся в окне редактирования.
+*
+* @return текст из окна редактирования.
+*/
+String getText() {
+// Реализация метода
+
+return text.getText();
+}
+
 }
--- DevJ120.Lab4/src/SimpleEditorListener.java
+++ DevJ120.Lab4/src/SimpleEditorListener.java
@@ -8,6 +8,174 @@
  *
  * @author bezdetk0@mail.ru
  */
-public class SimpleEditorListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+// Импорт классов, которые используются при решении данной задачи.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+/**
+* Класс поддерживает обработку событий WindowEvent и ActionEvent, возникающих в
+* классе SimpleEditor.
+*
+* @author (C)Y.D.Zakovryashin, 11.11.2020
+*/
+public class SimpleEditorListener extends WindowAdapter
+implements ActionListener, AutoCloseable {
+
+/**
+* Ссылка на главное окно приложения.
+*/
+private SimpleEditor editor;
+
+/**
+* Ссылка на текущий файл, открытый в приложении.
+*/
+private File file;
+
+/**
+* Ссылки на потоки ввода/вывода, связанные с текущим файлом.
+*/
+private FileReader reader;
+private FileWriter writer;
+
+/**
+* Конструктор класса, определяющий ссылку на главное окно приложения.
+*
+* @param editor ссылка на главное окно приложения.
+*/
+public SimpleEditorListener(SimpleEditor editor) {
+this.editor = editor;
+}
+
+/**
+* Метод обеспечивает обработку событий ActionEvent, связанных с кнопками и
+* пунктами меню класса SimpleEditor.
+*
+* @param ae ссылка на объект, описывающий событие.
+*/
+
+@Override
+public void actionPerformed(ActionEvent ae) {
+    switch (ae.getActionCommand()) {
+    case "open":
+// Обработка команды на открытие редактируемого файла. При
+// обработке этой команды следует использовать классы:
+// java.io.FileReader;
+// javax.swing.JFileChooser;
+// javax.swing.JOptionPane.
+// Следует учесть, что при подаче этой команды в приложении
+// уже может быть открыт какой-то файл.
+// Все выброшенные исключения в данном фрагменте должны быть
+// обработаны, при этом пользователю должны сообщаться причины
+// исключения с помощью стандартных диалоговых окон класса
+// JOptionPane
+            if (file == null){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Opening a file");
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            int res = fileChooser.showDialog(editor, "Select");
+            if (res == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+                try (FileReader fr = new FileReader(file)){
+                   char[] buffer = new char[1024];
+                   fr.read(buffer);
+                   while ( fr.read(buffer) != -1){
+                   editor.appendText(new String(buffer), false);
+                   buffer = new char[1024];
+                        }
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(editor, "File doesnt read", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                 }
+            }
+            else {
+                int input = JOptionPane.showConfirmDialog (null, "File already open. Save chanches?");
+                // 0=yes, 1=no, 2=cancel
+//                System. out .println(input);
+                if (input == 1){
+                    
+//                    ae.getActionCommand(case:"save");
+                }
+            }
+
+break;
+case "save":
+// Обработка команды на сохранение результатов редактирования
+// в открытом файле.
+if (file !=null)
+        try (FileWriter writer = new FileWriter(file, false)) {
+            String resultText = editor.getText();
+            writer.write(resultText);    
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(editor, "File can't be written", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
     
+break;
+case "cancel":
+// Обработка команды на закрытие открытого файла без сохранения
+// сделанных изменений.
+    file = null;
+    editor.appendText("", true);    
+    
+break;
+case "exit":
+// Обработка команды на закрытие приложения. При обработке этой
+// команды следует учесть, что в момент её подачи в приложении
+// может быть открыт какой-то файл. В этом случае пользователю
+// с помощью стандартного диалогового окна класса JOptionPane
+// должен предлагаться выбор между закрытием этого файла с
+// сохранением или без сохранения сделанных в этом файле
+// изменений.
+}
+}
+
+//private String open (){
+//            String text = "";
+//            JFileChooser fileChooser = new JFileChooser();
+//            fileChooser.setDialogTitle("Opening a file");
+//            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+//            int res = fileChooser.showDialog(editor, "Select");
+//            
+//            if (res == JFileChooser.APPROVE_OPTION) {
+//            File file = fileChooser.getSelectedFile();
+//            
+//            try (FileReader reader = new FileReader(file)){
+//            char[] buffer = new char[1024];
+//                   reader.read(buffer);
+//                   while ( reader.read(buffer) != -1){
+//                   text += new String(buffer);
+//                   buffer = new char[1024];
+//
+//           }
+//                   
+//            
+//            } catch (IOException ex) {
+//                JOptionPane errorPane = new JOptionPane();
+//                errorPane.showMessageDialog(null, "Something Went Wrong..", "Error as Title", JOptionPane.ERROR_MESSAGE);
+//            }
+//            }
+//            return text;
+//}
+
+/**
+* Метод, автоматически вызываемый при удалении объекта из памяти (обычно
+* при закрытии приложения). Следует учесть, что в в момент его вызова в
+* приложении может оставаться открытым какой-то файл.
+*
+*/
+@Override
+public void close() {
+throw new UnsupportedOperationException("Not supported yet.");
+}
 }
